accce09393613ed4c46769a79a0784567e1c19b1
Ziven <>
2023-04-30 22:41:50 +0800

    v0.1.7

--- js/src/forum/components/MoneyLeaderboardIndexPage.js
+++ js/src/forum/components/MoneyLeaderboardIndexPage.js
@@ -76,7 +76,7 @@ export default class MoneyLeaderboardIndexPage extends Page {
 
               {!this.loading && this.moneyLeaderboardList.length===0 && (
                 <div>
-                  <div style="font-size:1.4em;color: var(--muted-more-color);text-align: center;height: 300px;line-height: 100px;">{app.translator.trans("ziven-money-leaderboard.forum.leaderboard-list-money")}</div>
+                  <div style="font-size:1.4em;color: var(--muted-more-color);text-align: center;height: 300px;line-height: 100px;">{app.translator.trans("ziven-money-leaderboard.forum.leaderboard-list-empty")}</div>
                 </div>
               )}
 
